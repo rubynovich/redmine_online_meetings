d01fb936228dfc1e010b7f99ad558c4ece1505e8
Maksim Koritskiy <m.koritskiy@gorkapstroy.ru>
2013-12-05 15:31:28 +0400

    fix IssuePatch

--- init.rb
+++ init.rb
@@ -28,7 +28,7 @@ require 'concerns/online_meeting_mailer_patch'
 require 'concerns/issue_patch'
 require 'meeting_agendas_controller_patch'
 
-Issue.send(:include, Issue)
+Issue.send(:include, OnlineMeetingIssuePatch)
 Mailer.send(:include, OnlineMeetingMailerPatch)
 MeetingAgenda.send(:include, OnlineMeetingAgendaPatch)
 MeetingAgendasController.send(:include, OnlineMeetings::MeetingAgendasControllerPatch) unless MeetingAgendasController.included_modules.include? OnlineMeetings::MeetingAgendasControllerPatch
