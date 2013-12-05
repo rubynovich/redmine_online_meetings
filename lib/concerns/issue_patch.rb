29015ecc8585ec557f026f8f6641245041841654
Maksim Koritskiy <m.koritskiy@gorkapstroy.ru>
2013-12-05 15:27:44 +0400

    issue only meeting

--- lib/concerns/issue_patch.rb
+++ lib/concerns/issue_patch.rb
@@ -7,7 +7,7 @@ module OnlineMeetingIssuePatch
   def failred_remove_from_meeting
     if self.status_id == (Setting[:plugin_redmine_online_meetings][:failure_issue_status] || nil).try(:to_i)
       #calendar_update
-      meeting_member.meeting_agenda.add_calendar_event
+      meeting_member.meeting_agenda.add_calendar_event if meeting_member
       return true
     end
   end
