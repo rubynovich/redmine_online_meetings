48c6449aba7c27929d26f18ba6f5e843f5411fea
Maksim Koritskiy <m.koritskiy@gorkapstroy.ru>
2013-12-05 15:29:57 +0400

    fix issue path

--- lib/concerns/issue_patch.rb
+++ lib/concerns/issue_patch.rb
@@ -1,13 +1,13 @@
 module OnlineMeetingIssuePatch
   extend ActiveSupport::Concern
   included do
-    after_update :failred_remove_from_meeting
+    after_update :failred_remove_from_meeting, if: -> { self.meeting_member }
   end
 
   def failred_remove_from_meeting
     if self.status_id == (Setting[:plugin_redmine_online_meetings][:failure_issue_status] || nil).try(:to_i)
       #calendar_update
-      meeting_member.meeting_agenda.add_calendar_event if meeting_member
+      meeting_member.meeting_agenda.add_calendar_event
       return true
     end
   end
