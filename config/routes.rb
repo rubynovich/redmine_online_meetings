2628f325020cf2a74e8cb083843f5e78ce13941c
Maksim <max@koritskiy.ru>
2014-01-09 16:44:11 +0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-resources :meeting_agendas do
-  member do
-    get 'issue'
-    get 'start_record'
-    get 'stop_record'
-    post 'continue_record'
-  end
-end
+#resources :meeting_agendas do
+#  member do
+get 'meeting_agendas/:id/issue' => 'meeting_agendas#issue'
+get 'meeting_agendas/:id/start_record' => 'meeting_agendas#start_record'
+get 'meeting_agendas/:id/stop_record' => 'meeting_agendas#stop_record'
+post 'meeting_agendas/:id/continue_record' => 'meeting_agendas#continue_record'
+#  end
+#end
